1b8920461526f514ac66f75dd99fd3dca84242fd
gilgamesh7 <felidaesrule@yahoo.com>
2023-08-13 15:21:06 +1200

    Human message chat prompt

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # langchain_intro
-
+- [LangChain is AMAZING | Quick Python Tutorial](https://www.youtube.com/watch?v=I4mFqyqFkxg)
 # Installations
 - pip install python-dotenv
 - pip install langchain
--- main.py
+++ main.py
@@ -5,11 +5,17 @@ import logging
 
 from langchain.llms import OpenAI # Pre 3.5 models
 from langchain.chat_models import ChatOpenAI # modern models
+from langchain.prompts.chat import(
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate
+)
 
 load_dotenv()
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 OPENAI_MODEL = os.environ.get('OPENAI_MODEL')
-
+PROMPT_COUNTRY_INFO = """
+    Provide information about {country}.
+"""
 def setup()-> str:
     try:
         logging.basicConfig(level=logging.INFO, format="[{asctime}] - {funcName} - {lineno} - {message}", style='{')
@@ -26,6 +32,9 @@ def main():
         logger = setup()
         logger.info("Starting langchain Intro")
 
+        '''
+            Simple - pre 3.5 models
+        '''
         logger.info("Create llm model for older GPT models , pre < 3.5")
         older_llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 
@@ -41,6 +50,9 @@ def main():
         )
         logger.info(f"Prediction from pre 3.5 models : {result}")
 
+        '''
+            Simple 3.5 + Models
+        '''
         llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL)
         logger.info(f"Using {OPENAI_MODEL} model : Give me 5 facts about Rajinikanth")
         result = llm.predict(
@@ -54,6 +66,17 @@ def main():
         )
         logger.info(f"Prediction from {OPENAI_MODEL} model : {result}")
 
+
+        '''
+            Using Prompts
+        '''
+        country = input("Enter country name : ")
+        message = HumanMessagePromptTemplate.from_template(template=PROMPT_COUNTRY_INFO)
+        chat_prompt = ChatPromptTemplate.from_messages(messages=[message])
+        chat_prompt_with_values = chat_prompt.format_prompt(country=country)
+        response = llm(chat_prompt_with_values.to_messages())
+
+        logger.info(f"Human prompt + Chat Prompt Template : {response}")
 
     except Exception as error:
         logger.error(f"{type(error).__name__} - {error}")   
