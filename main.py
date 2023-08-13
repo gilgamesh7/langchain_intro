95ae380bc55342396b7bca2ab48c4c2ca1a51f5d
gilgamesh7 <felidaesrule@yahoo.com>
2023-08-13 13:57:13 +1200

    Use gpt 3.5+ models

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import os
 from dotenv import load_dotenv
 import logging
 
-from langchain.llms import OpenAI
+from langchain.llms import OpenAI # Pre 3.5 models
+from langchain.chat_models import ChatOpenAI # modern models
 
 load_dotenv()
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+OPENAI_MODEL = os.environ.get('OPENAI_MODEL')
 
 def setup()-> str:
     try:
@@ -24,14 +26,35 @@ def main():
         logger = setup()
         logger.info("Starting langchain Intro")
 
-        logger.info("Create llm model")
-        llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+        logger.info("Create llm model for older GPT models , pre < 3.5")
+        older_llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 
-        logger.info("Q1 : Give me 5 facts about Rajinikanth")
+        logger.info("Using pre 3.5 : Give me 5 facts about Rajinikanth")
+        result = older_llm.predict(
+            "Give me 5 facts about Rajinikanth"
+        )
+        logger.info(f"Prediction from pre 3.5 models : {result}")
+
+        logger.info("Using pre 3.5 : Who is the Prime Minister of New Zealand")
+        result = older_llm.predict(
+            "Who is the Prime Minister of New Zealand"
+        )
+        logger.info(f"Prediction from pre 3.5 models : {result}")
+
+        llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL)
+        logger.info(f"Using {OPENAI_MODEL} model : Give me 5 facts about Rajinikanth")
         result = llm.predict(
             "Give me 5 facts about Rajinikanth"
         )
-        logger.info(result)
+        logger.info(f"Prediction from {OPENAI_MODEL} model : {result}")
+
+        logger.info(f"Using {OPENAI_MODEL} model : Who is the Prime Minister of New Zealand")
+        result = llm.predict(
+            "Who is the Prime Minister of New Zealand"
+        )
+        logger.info(f"Prediction from {OPENAI_MODEL} model : {result}")
+
+
     except Exception as error:
         logger.error(f"{type(error).__name__} - {error}")   
         raise error 
